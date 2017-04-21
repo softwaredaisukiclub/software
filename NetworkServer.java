1ad66057ee14f0511063316dba655697ccec2605
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-04-21 16:13:26 +0900

    [update]親クラスのひな形作成

---
+++ NetworkServer.java
@@ -0,0 +1,22 @@
+public class NetworkServer{
+	public NetworkServer(String myaddress, String[] addressies){
+		public String myaddress = address
+		public String[] addressies = addressies
+	}
+
+	public String zip(File data){
+		//ファイルを圧縮するメソッド
+	}
+
+	public File unzip(String data){
+			//ファイルを解凍するメソッド
+	}
+
+	public void send(String message){
+		//文字列を送信するメソッド
+	}
+
+	public String get(){
+		//文字列を受信するメソッド
+	}
+}
