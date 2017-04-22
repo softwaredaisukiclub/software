4c530b128359c3fada86269a6bfcc6f864172425
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-04-23 00:12:47 +0900

    [update]インデントの変更

--- NetworkServer.java
+++ NetworkServer.java
@@ -1,7 +1,7 @@
 package software;
 import java.util.*;
 import java.io.*;
-public class NetworkServer{
+public class NetworkServer {
 	//送受信されたデータを保存するディレクトリを設定するクラス変数
 	private static String send_data_dir = "send_data/";
 	private static String input_data_dir = "input_data/";
@@ -31,6 +31,7 @@ public class NetworkServer{
 	public static File unzip(File file) throws Exception {
 		String filename = file.getName();
 		return ZipClient.decompressZip(send_data_dir+filename, get_data_dir)[0];
+	}
 /*
 public void send(File file, String filename) {
 		//文字列を送信するメソッド
--- ZipClient.java
+++ ZipClient.java
@@ -3,68 +3,67 @@ import java.util.zip.*;
 import java.util.*;
 import java.io.*;
 public final class ZipClient {
-  public static File compressZip( String rootPath, String[] inputFiles , String outputFile ) throws Exception {
-
-    if( rootPath     == null  ){ throw new Exception(); }
-    if( inputFiles   == null ){ throw new Exception(); }
-    if( outputFile   == null ){ throw new Exception(); }
-    try(
-     FileOutputStream        out     = new FileOutputStream(outputFile);
-     BufferedOutputStream    bos     = new BufferedOutputStream(out);
-     ZipOutputStream         archive = new ZipOutputStream(out); 
-     ){
-     for( String fileName : inputFiles ) {
-      ZipEntry entry   = new ZipEntry(fileName);
-      archive.putNextEntry( entry );
-      if( fileName.endsWith("/") ) { 
-       archive.closeEntry();
-       continue;
-     }
-     try(    
-       FileInputStream     fis = new FileInputStream(rootPath+fileName);
-       BufferedInputStream bis = new BufferedInputStream(fis);
-       ){
-       int     size = 0;
-       byte[]  buf = new byte[1024];
-       while( (size = bis.read(buf)) > 0 ) {
-        archive.write(buf, 0, size );
-      }
-    }
-    archive.closeEntry();
-  }
-}
-return new File(outputFile);
-}
-//ファイルをデコードするアプリ
-public static File[] decompressZip( String inputFile , String outputDir ) throws Exception {
-  ArrayList<File> files = new ArrayList<File>();
-  try(   
-   FileInputStream fis = new FileInputStream(inputFile);
-   ZipInputStream  archive = new ZipInputStream(fis);
-   ){
-          // エントリーを1つずつファイル・フォルダに復元
-   ZipEntry entry   = null;
-   while( ( entry = archive.getNextEntry() ) != null ) {
-    File file = new File( outputDir + "/" + entry.getName() );
-    if( entry.isDirectory() ) {
-     file.mkdirs();
-     continue;
-   }
-   if( !file.getParentFile().exists() ){ file.getParentFile().mkdirs(); }
-   try(   
-     FileOutputStream fos = new FileOutputStream( file );
-     BufferedOutputStream bos = new BufferedOutputStream( fos ); 
-     ){
-     int     size    = 0;
-     byte[]  buf     = new byte[ 1024 ];
-     while( ( size = archive.read( buf ) ) > 0 ) {
-      bos.write( buf, 0, size );
-    }
-    files.add(file);
-  }
-}
-}
-return (File[])files.toArray(new File[0]);
-}
+	public static File compressZip( String rootPath, String[] inputFiles , String outputFile ) throws Exception {
+    //ファイルを圧縮するメソッド
+		if( rootPath     == null ){ throw new Exception(); }
+		if( inputFiles   == null ){ throw new Exception(); }
+		if( outputFile   == null ){ throw new Exception(); }
+		try(
+			FileOutputStream     out     = new FileOutputStream(outputFile);
+			BufferedOutputStream bos     = new BufferedOutputStream(out);
+			ZipOutputStream      archive = new ZipOutputStream(out); 
+			){
+			for( String fileName : inputFiles ) {
+				ZipEntry entry   = new ZipEntry(fileName);
+				archive.putNextEntry( entry );
+				if( fileName.endsWith("/") ) { 
+					archive.closeEntry();
+					continue;
+				}
+				try(
+					FileInputStream     fis = new FileInputStream(rootPath+fileName);
+					BufferedInputStream bis = new BufferedInputStream(fis);
+					){
+					int     size = 0;
+					byte[]  buf = new byte[1024];
+					while( (size = bis.read(buf)) > 0 ) {
+						archive.write(buf, 0, size );
+					}
+				}
+				archive.closeEntry();
+			}
+		}
+		return new File(outputFile);
+	}
+//ファイルを解凍するメソッド
+	public static File[] decompressZip( String inputFile , String outputDir ) throws Exception {
+		ArrayList<File> files = new ArrayList<File>();
+		try(   
+			FileInputStream fis = new FileInputStream(inputFile);
+			ZipInputStream  archive = new ZipInputStream(fis);
+			){
+			ZipEntry entry   = null;
+			while( ( entry = archive.getNextEntry() ) != null ) {
+				File file = new File( outputDir + "/" + entry.getName() );
+				if( entry.isDirectory() ) {
+					file.mkdirs();
+					continue;
+				}
+				if( !file.getParentFile().exists() ){ file.getParentFile().mkdirs(); }
+				try(   
+					FileOutputStream fos = new FileOutputStream( file );
+					BufferedOutputStream bos = new BufferedOutputStream( fos ); 
+					){
+					int     size = 0;
+					byte[]  buf  = new byte[ 1024 ];
+					while( (size = archive.read(buf)) > 0 ) {
+						bos.write( buf, 0, size );
+					}
+					files.add(file);
+				}
+			}
+		}
+		return (File[])files.toArray(new File[0]);
+	}
 
 }
